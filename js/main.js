206c843b6e071d28fab1d849e8ef414e4729b010
Ying <whydang@uw.edu>
2015-10-29 09:57:49 -0700

    ahaha :D

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 // Create an angular application called "sortApp"
+var app = angular.module('sortApp', []);
 
 
 // Define a controller "mainController" for you application, 
 // making sure to pass in both the $scope and $http variables
-
+    sortApp.controller(function($scope, $http) {
+        $http.get('data/salaries.json').then(function(response){
+            $scope.data = response.data;
+        });
+    });
 	// Use and $http.get request to get the salary data
 	
 		// Set $scope.data equal to the data returned in your results
